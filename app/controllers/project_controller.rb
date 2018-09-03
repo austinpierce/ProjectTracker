ad7d8bb595cd56519c3b2409be86feb244b5a5c9
Austin Pierce <AustinPierce@gmail.com>
2018-09-02 22:20:45 -0500

    Create project

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,3 +1,27 @@
 class ProjectController < ApplicationController
 
+	def new
+		@project = Project.new
+	end
+
+	def create 
+		@project = Project.new(project_params)
+		if @project.save
+			flash[:success] = "Project was created"
+			redirect_to project_index_path
+		else
+			render 'new'
+		end
+	end
+
+	def index
+		@projects = Project.all
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:name, :short_name, :description)
+	end
+
 end
