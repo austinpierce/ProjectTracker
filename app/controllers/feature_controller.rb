1e8e0907fbd5f2ace0f1d94c449ed2eea7042f9c
Austin Pierce <AustinPierce@gmail.com>
2018-09-03 00:31:33 -0500

    Create feature

--- app/controllers/feature_controller.rb
+++ app/controllers/feature_controller.rb
@@ -1,3 +1,30 @@
 class FeatureController < ApplicationController
 
+	def new
+		@feature = Feature.new
+		@projects = Project.all
+	end
+
+	def create
+		@feature = Feature.new(feature_params)
+		if @feature.save
+			flash[:success] = "Feature was created"
+			redirect_to feature_index_path
+		else
+			render 'new'
+		end
+	end
+
+	def index
+		@features = Feature.all
+	end
+
+	private
+
+	def feature_params
+		params.require(:feature).permit(:project_id, :title, 
+			:description, :estimate, :priority, :dependant_on_code,
+			:is_complete, :time_spent, :git_merge_path) 	
+	end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
 	root 'welcome#index'
 	resources :project
+	resources :feature
 
 end
